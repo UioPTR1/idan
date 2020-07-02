8629719c5f2748d360361de6a731e62ba8150147
UioPTR1 <itaymailb@gmail.com>
2020-07-02 21:21:41 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,111 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div class="content">{{ word }}</div>
+    <button @click="clicked">תלחצייי על זההה</button>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      array: [
+        "איש",
+        "‎הר",
+        "‎מדינה",
+        "‎אוקינוס",
+        "‎מחוז",
+        "‎בניין",
+        "‎חתול",
+        "‎חברת תעופה",
+        "‎אהב",
+        "‎עושר",
+        "‎אושר",
+        "‎גאווה",
+        "‎פחד",
+        "‎דת",
+        "‎אמונה",
+        "היסטוריה",
+        "‎תקשורת",
+        "‎בית",
+        "‎אוקיינוס",
+        "‎הדוד מייק",
+        "‎ציפור",
+        "‎צילום",
+        "‎בננה",
+        "‎עיניים",
+        "אור",
+        "‎שמש",
+        "‎כלב",
+        "‎מזוודה",
+        "‎פרחים",
+        "‎מיטה",
+        "‎חתול",
+        "‎סרט",
+        "‎רכבת",
+        "‎מדינה",
+        "‎ספר",
+        "‎טלפון",
+        "‎משחק",
+        "‎רמקול",
+        "‎שעון‎עט",
+        "‎כינור",
+        "חלב",
+        "‎אוכל",
+        "‎מוזיקה",
+        "‎מפה",
+        "‎משקפיים",
+        "‎ניו יורק",
+        "‎תמונה",
+        "‎כלה",
+        "‎צמות",
+        "‎אור שמש",
+        "בובספוג",
+        "סופר מן",
+        "ראש העיר",
+        "ראש הממשלה",
+        "כוכב",
+        "חללית",
+        "קרמבולה"
+      ],
+      word: "loading"
+    };
+  },
+  created() {
+    this.word = this.array[Math.floor(Math.random() * this.array.length)];
+  },
+  methods: {
+    clicked() {
+      this.word = this.array[Math.floor(Math.random() * this.array.length)];
+    }
+  }
+};
+</script>
+
 <style>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #dae1e7;
+  background-color: #142850;
 }
 
-#nav {
-  padding: 30px;
+button {
+  margin-top: 1rem;
+  background-color: #00909e; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 26px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.content {
+  margin-top: 5rem;
+  font-size: 3rem;
 }
 </style>
